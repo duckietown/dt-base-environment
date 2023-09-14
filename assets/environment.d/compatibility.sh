eb8b3f7027a75cb3b9a94e22356a91ff9f00ee6f
Andrea F. Daniele <afdaniele@duckietown.com>
2023-09-14 16:45:36 -0400

    renamed variables DT_MODULE_ -> DT_PROJECT_

--- assets/environment.d/compatibility.sh
+++ assets/environment.d/compatibility.sh
@@ -1,5 +1,5 @@
 # we set these environment variables for backward compatibility with code from DTProjects v3 and older
 
-export DT_MAINTAINER="${DT_MODULE_MAINTAINER}"
-export DT_REPO_PATH="${DT_MODULE_PATH}"
-export DT_LAUNCH_PATH="${DT_MODULE_LAUNCHERS_PATH}"
+export DT_MAINTAINER="${DT_PROJECT_MAINTAINER}"
+export DT_REPO_PATH="${DT_PROJECT_PATH}"
+export DT_LAUNCH_PATH="${DT_PROJECT_LAUNCHERS_PATH}"
