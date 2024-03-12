a784372f408be571508728711511286f8fa357f9
Andrea F. Daniele <afdaniele@duckietown.com>
2024-03-12 10:35:12 -0400

    minor

--- assets/environment.d/colcon.sh
+++ assets/environment.d/colcon.sh
@@ -1,1 +1,7 @@
-source /opt/colcon/install/setup.bash
+COLCON_SETUP_FPATH=/opt/colcon/install/setup.bash
+
+if [ -f "${COLCON_SETUP_FPATH}" ]; then
+    source "${COLCON_SETUP_FPATH}"
+else
+    echo "WARNING: colcon setup file not found: ${COLCON_SETUP_FPATH}"
+fi
